0727caad018deca80115d131be71d59d72830613
Sachin <sachinlakshan04@gmail.com>
2023-07-19 20:41:05 +0530

    Brand logo updated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Heading } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
@@ -8,7 +8,7 @@ const NavBar = () => {
     <HStack justifyContent="space-between" padding="10px">
       <Box display="flex" alignItems="center">
         <Image src={logo} alt="Dan Abramov" boxSize="60px" />
-        <Text>Game-Hub</Text>
+        <Heading size={{ base: "sm", md: "md", lg: "lg" }}>Game-Hub</Heading>
       </Box>
       <ColorModeSwitch />
     </HStack>
